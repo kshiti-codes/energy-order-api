f2cb7876406d475a9ff0863d66fccc8a28292a9d
kshiti-codes <kshiti.de@gmail.com>
2026-03-04 11:20:43 +0100

    build issue resolution

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log('EnergyOrder API running on http://localhost:3000');
 }
 bootstrap();
